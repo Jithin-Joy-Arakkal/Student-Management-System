6330973ef78caa29ad40193559772aac60b5a4e8
Jithu630 <jithuk0337@gmail.com>
2026-05-05 01:13:22 +0530

    Update db.php

--- sms-project/config/db.php
+++ sms-project/config/db.php
@@ -1,8 +1,9 @@
 <?php
-$host = "aws-1-ap-northeast-1.pooler.supabase.com";   // 🔁 replace with your host
+$host = "aws-1-ap-northeast-1.pooler.supabase.com";   // 🔁 your Supabase host
 $dbname = "postgres";
 $user = "postgres.ezlrihbweqznymvwkwxm";
-$pass = "s4csa24101109";          // 🔁 replace
+$pass = "s4csa24101109";          // 🔁 your Supabase password
+
 
 try {
     $conn = new PDO(
